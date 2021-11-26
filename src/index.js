099f7f093e1e1f6c36f0e026c06044a22c65c2a7
aimner <aimner510@gmail.com>
2021-11-26 23:37:56 +0300

    refactoring state

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import state from './state/state';
+import store from './redux/state';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { newPost, newMessage, changePost, changeMessage, call } from './state/state';
 
 
 
-export let renderApp = (state) => {
+
+export let renderApp = () => {
     ReactDOM.render(
-      
-        <App state = {state} newpost = {newPost} newmessage= {newMessage} changepost={changePost} changemessage={changeMessage}/>,
+        <App state = {store._state} newpost = {store.newPost.bind(store)} newmessage= {store.newMessage.bind(store)} changepost={store.changePost.bind(store)} changemessage={store.changeMessage.bind(store)}/>,
       document.getElementById('root')
     )};
 
-renderApp(state);
-call(renderApp);
+renderApp(store.getState());
+store.call(renderApp);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
